cb5fea17872b677adab5ade8a12be346633f5a12
Rémi Bruyère <remi.bruyere@affluences.com>
2020-07-01 19:32:27 +0200

    Fix file selector

--- lib/View/files.dart
+++ lib/View/files.dart
@@ -25,22 +25,19 @@ class FilesPage extends StatefulWidget {
   _FilesPageState createState() => _FilesPageState();
 }
 
-class _FilesPageState extends State<FilesPage>{
+class _FilesPageState extends State<FilesPage> {
 
   String _fileName;
   String _path;
   Map<String, String> _paths;
-  String _extension;
   bool _loadingPath = false;
   bool _multiPick = false;
   bool _hasValidMime = false;
-  FileType _pickingType;
-  TextEditingController _controller = new TextEditingController();
+  FileType _pickingType = FileType.any;
 
   @override
   void initState() {
     super.initState();
-    _controller.addListener(() => _extension = _controller.text);
   }
 
   void _openFileExplorer() async {
@@ -50,11 +47,11 @@ class _FilesPageState extends State<FilesPage>{
         if (_multiPick) {
           _path = null;
           _paths = await FilePicker.getMultiFilePath(
-              type: _pickingType, fileExtension: _extension);
+              type: _pickingType);
         } else {
           _paths = null;
           _path = await FilePicker.getFilePath(
-              type: _pickingType, fileExtension: _extension);
+              type: _pickingType);
         }
       } on PlatformException catch (e) {
         print("Unsupported operation" + e.toString());
@@ -63,7 +60,9 @@ class _FilesPageState extends State<FilesPage>{
       setState(() {
         _loadingPath = false;
         _fileName = _path != null
-            ? _path.split('/').last
+            ? _path
+            .split('/')
+            .last
             : _paths != null ? _paths.keys.toString() : '...';
       });
     }
@@ -71,64 +70,63 @@ class _FilesPageState extends State<FilesPage>{
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("FilePickerApp"),
-        ),
-        body: Container(
-          child: Center(
-              child: Column(
-                children: <Widget>[
-                  new Padding(
-                    padding: const EdgeInsets.only(top: 50.0, bottom: 20.0),
-                    child: new RaisedButton(
-                      onPressed: () => _openFileExplorer(),
-                      child: new Text("Open file picker"),
-                    ),
+    return Scaffold(
+      body: Container(
+        child: Center(
+            child: Column(
+              children: <Widget>[
+                new Padding(
+                  padding: const EdgeInsets.only(top: 50.0, bottom: 20.0),
+                  child: new RaisedButton(
+                    onPressed: () => _openFileExplorer(),
+                    child: new Text("Open file picker"),
                   ),
-                  new Builder(
-                    builder: (BuildContext context) => _loadingPath
-                        ? Padding(
-                        padding: const EdgeInsets.only(bottom: 10.0),
-                        child: const CircularProgressIndicator())
-                        : _path != null || _paths != null
-                        ? new Container(
-                      padding: const EdgeInsets.only(bottom: 30.0),
-                      height: MediaQuery.of(context).size.height * 0.50,
-                      child: new Scrollbar(
-                          child: new ListView.separated(
-                            itemCount: _paths != null && _paths.isNotEmpty
-                                ? _paths.length
-                                : 1,
-                            itemBuilder: (BuildContext context, int index) {
-                              final bool isMultiPath =
-                                  _paths != null && _paths.isNotEmpty;
-                              final String name = 'File $index: ' +
-                                  (isMultiPath
-                                      ? _paths.keys.toList()[index]
-                                      : _fileName ?? '...');
-                              final path = isMultiPath
-                                  ? _paths.values.toList()[index].toString()
-                                  : _path;
+                ),
+                new Builder(
+                  builder: (BuildContext context) =>
+                  _loadingPath
+                      ? Padding(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      child: const CircularProgressIndicator())
+                      : _path != null || _paths != null
+                      ? new Container(
+                    padding: const EdgeInsets.only(bottom: 30.0),
+                    height: MediaQuery
+                        .of(context)
+                        .size
+                        .height * 0.50,
+                    child: new Scrollbar(
+                        child: new ListView.separated(
+                          itemCount: _paths != null && _paths.isNotEmpty
+                              ? _paths.length
+                              : 1,
+                          itemBuilder: (BuildContext context, int index) {
+                            final bool isMultiPath =
+                                _paths != null && _paths.isNotEmpty;
+                            final String name = 'File $index: ' +
+                                (isMultiPath
+                                    ? _paths.keys.toList()[index]
+                                    : _fileName ?? '...');
+                            final path = isMultiPath
+                                ? _paths.values.toList()[index].toString()
+                                : _path;
 
-                              return new ListTile(
-                                title: new Text(
-                                  name,
-                                ),
-                                subtitle: new Text(path),
-                              );
-                            },
-                            separatorBuilder:
-                                (BuildContext context, int index) =>
-                            new Divider(),
-                          )),
-                    )
-                        : new Container(),
-                  ),
-                ],
-              )),
-        ),
+                            return new ListTile(
+                              title: new Text(
+                                name,
+                              ),
+                              subtitle: new Text(path),
+                            );
+                          },
+                          separatorBuilder:
+                              (BuildContext context, int index) =>
+                          new Divider(),
+                        )),
+                  )
+                      : new Container(),
+                ),
+              ],
+            )),
       ),
     );
   }
