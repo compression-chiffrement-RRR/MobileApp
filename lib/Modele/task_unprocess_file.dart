0b95005c260bf242fe255fe7f3f2016e339dd062
Rémi Bruyère <remi.bruyere@affluences.com>
2020-07-14 01:37:30 +0200

    Finish download page

--- lib/Modele/task_unprocess_file.dart
+++ lib/Modele/task_unprocess_file.dart
@@ -4,4 +4,16 @@ class TaskUnprocessFile {
   List<UnprocessFile> types;
 
   TaskUnprocessFile(this.types);
+
+  Map<String, dynamic> toJson() {
+    if (types.length > 0) {
+      return {
+        'types': types.map((e) => e.toJson()).toList()
+      };
+    } else {
+      return {
+        'types': []
+      };
+    }
+  }
 }
--- lib/View/file_downloader.dart
+++ lib/View/file_downloader.dart
@@ -1,10 +1,15 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:mobileappflutter/Helper/dialog_helper.dart';
 import 'package:mobileappflutter/Modele/Enum/cipher_task_type.dart';
 import 'package:mobileappflutter/Modele/file_advanced_information.dart';
 import 'package:mobileappflutter/Modele/file_basic_information.dart';
+import 'package:mobileappflutter/Modele/task_unprocess_file.dart';
+import 'package:mobileappflutter/Modele/unprocess_file.dart';
 import 'package:mobileappflutter/Service/file_service.dart';
+import 'package:mobileappflutter/Service/worker_service.dart';
 import 'package:mobileappflutter/Style/color.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 import 'package:enum_to_string/enum_to_string.dart';
@@ -20,26 +25,38 @@ class FileDownloaderPage extends StatefulWidget {
 
 class _FileDownloaderPage extends State<FileDownloaderPage> {
   final FileService _fileService = FileService();
+  final WorkerService _workerService = WorkerService();
 
   final _scaffoldKey = GlobalKey<ScaffoldState>();
   final _formKey = GlobalKey<FormState>();
   bool _askUnprocessFile = false;
+  bool _unprocessFile = false;
   bool _downloadingFile = false;
   bool _isDownloaded = false;
   String _progress = '0';
-  List<Widget> formPasswordInputs = new List();
+  List<Widget> _formPasswordInputs = new List();
+  TaskUnprocessFile _taskUnprocessFile = TaskUnprocessFile([]);
 
   final FileBasicInformation _fileBasicInformation;
   FileAdvancedInformation _fileAdvancedInformation;
+  FileBasicInformation _fileUnprocessed;
 
   _FileDownloaderPage(this._fileBasicInformation);
+
+  Timer _timer;
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
       appBar: AppBar(
-        elevation: 0,
+        elevation: 1,
         brightness: Brightness.dark,
         backgroundColor: Colors.white,
         leading: IconButton(
@@ -65,23 +82,25 @@ class _FileDownloaderPage extends State<FileDownloaderPage> {
                   fontWeight: FontWeight.bold),
             ),
             !_fileBasicInformation.isTreated ? Icon(Icons.not_interested, color: Colors.black): Container(),
-            SizedBox(
-              height: 30,
+            Divider(
+              color: Colors.black,
+              height: 50,
+              thickness: 1,
+              indent: 70,
+              endIndent: 70,
             ),
             Center(
               child: makeButtonDownloadWithProcessApplied(_fileBasicInformation),
             ),
-            Divider(
-              color: Colors.black,
+            SizedBox(
               height: 50,
-              thickness: 1,
-              indent: 70,
-              endIndent: 70,
             ),
             Center(
-              child: makeButtonDownloadWithoutProcessApplied(_fileBasicInformation),
+              child: !_askUnprocessFile ?
+                makeButtonDownloadWithoutProcessApplied(_fileBasicInformation):
+                makeButtonValidateDownloadWithProcessApplied(_fileAdvancedInformation),
             ),
-            makeFormUnprocess(_fileAdvancedInformation),
+            _askUnprocessFile && !_downloadingFile ? makeFormUnprocess(_fileAdvancedInformation) : Container(),
             Divider(
               color: Colors.black,
               height: 50,
@@ -89,7 +108,8 @@ class _FileDownloaderPage extends State<FileDownloaderPage> {
               indent: 70,
               endIndent: 70,
             ),
-            _downloadingFile ? makeContainerShowStatusFileDownload() : Container(),
+            _unprocessFile ? makeContainerShowStatusFileUnprocess() : Container(),
+            _downloadingFile || _isDownloaded ? makeContainerShowStatusFileDownload() : Container(),
             _isDownloaded ? makeSuccessMessage() : Container(),
           ],
         ),
@@ -107,36 +127,30 @@ class _FileDownloaderPage extends State<FileDownloaderPage> {
         padding: EdgeInsets.symmetric(horizontal: 80, vertical: 13),
         child: Text("Download with process applied", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
         onPressed: () async {
-          String path = await _fileService.downloadFile(fileBasicInformation, (rcv, total) {
-            print(
-                'received: ${rcv.toStringAsFixed(0)} out of total: ${total.toStringAsFixed(0)}');
-
-            setState(() {
-              _progress = ((rcv / total) * 100).toStringAsFixed(0);
-            });
-
-            if (_progress == '100') {
-              setState(() {
-                _isDownloaded = true;
-              });
-            } else if (double.parse(_progress) < 100) {}
-          });
-          if (path == null) {
-            DialogHelper.displayDialog(context, "Download", "Cannot download file yet, please retry later");
-            setState(() {
-              _progress = '0';
-              _isDownloaded = false;
-              _downloadingFile = false;
-            });
-          } else {
-            setState(() {
-              _progress = '0';
-              _isDownloaded = false;
-              _downloadingFile = true;
-            });
-          }
+          downloadFile(fileBasicInformation);
         },
       );
+
+  void downloadFile(FileBasicInformation fileBasicInformation) async {
+    String path = await _fileService.downloadFile(fileBasicInformation, (rcv, total) {
+      print(
+          'received: ${rcv.toStringAsFixed(0)} out of total: ${total.toStringAsFixed(0)}');
+
+      setState(() {
+        _progress = ((rcv / total) * 100).toStringAsFixed(0);
+      });
+
+      if (_progress == '100') {
+        setStateFinishDownload();
+      } else if (double.parse(_progress) < 100) {}
+    });
+    if (path == null) {
+      DialogHelper.displayDialog(context, "Download", "Cannot download file yet, please retry later");
+      setStateReset();
+    } else {
+      setStateDownloading();
+    }
+  }
 
   RaisedButton makeButtonDownloadWithoutProcessApplied(FileBasicInformation fileBasicInformation) =>
       RaisedButton(
@@ -149,24 +163,142 @@ class _FileDownloaderPage extends State<FileDownloaderPage> {
         child: Text("Download without process applied", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
         onPressed: () async {
           _fileAdvancedInformation = await _fileService.getInformation(_fileBasicInformation.uuid);
-          formPasswordInputs = new List();
-          List<String> cipherNames = EnumToString.toList(CipherTaskType.values);
-          _fileAdvancedInformation.processes.forEach((element) {
-            print(element.processType);
-            if (cipherNames.contains(element.processType)) {
-              formPasswordInputs.add(makeInput(
-                label: "${element.processType} n°${element.order} password",
-                obscureText: true
-              ));
+          if (_fileAdvancedInformation.isTreated) {
+            _formPasswordInputs = new List();
+            List<String> cipherNames = EnumToString.toList(CipherTaskType.values);
+            _fileAdvancedInformation.processes.forEach((process) {
+              print(process.processType);
+              if (cipherNames.contains(process.processType)) {
+                _formPasswordInputs.add(makeInput(
+                    label: "${process.processType} n°${process.order} password",
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    validator: (val) => val.isEmpty ? "Please entrer your password used during upload" : null,
+                    onSaved: (val) {
+                      _taskUnprocessFile.types.removeWhere((unprocessElement) => unprocessElement.uuid == process.uuid);
+                      _taskUnprocessFile.types.add(UnprocessFile(process.uuid, val));
+                    }
+                ));
+              }
+            });
+            if (_formPasswordInputs.length == 0) {
+              setStateUnprocessStatus();
+              _fileUnprocessed = await requestUnprocessFile(_fileAdvancedInformation, _taskUnprocessFile);
+              if (_fileUnprocessed == null) {
+                DialogHelper.displayDialog(context, "Download", "Cannot unprocess file yet, please retry later");
+                setStateReset();
+              } else {
+                checkIfFileIsTreated(_fileUnprocessed);
+              }
+            } else {
+              setStateAskUnprocessInformation();
             }
-          });
-          setState(() {
-            _progress = '0';
-            _isDownloaded = false;
-            _downloadingFile = false;
-          });
+          } else {
+            DialogHelper.displayDialog(context, "Download", "Cannot download file yet, please retry later");
+            setStateReset();
+          }
         },
       );
+
+  RaisedButton makeButtonValidateDownloadWithProcessApplied(FileBasicInformation fileBasicInformation) =>
+      RaisedButton(
+        textColor: Colors.white,
+        color: Colors.lightGreen,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 65, vertical: 13),
+        child: Text("Unprocess file with this information", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+        onPressed: () async {
+          if (_formKey.currentState.validate()) {
+            _formKey.currentState.save();
+            setStateUnprocessStatus();
+            _fileUnprocessed = await requestUnprocessFile(_fileAdvancedInformation, _taskUnprocessFile);
+            if (_fileUnprocessed == null) {
+              DialogHelper.displayDialog(context, "Download", "Cannot unprocess file yet, please retry later");
+              setStateReset();
+            } else {
+              checkIfFileIsTreated(_fileUnprocessed);
+            }
+          }
+        },
+      );
+
+  void setStateAskUnprocessInformation() {
+    setState(() {
+      _progress = '0';
+      _askUnprocessFile = true;
+      _isDownloaded = false;
+      _downloadingFile = false;
+      _unprocessFile = false;
+    });
+  }
+
+  void setStateUnprocessStatus() {
+    setState(() {
+      _progress = '0';
+      _askUnprocessFile = false;
+      _isDownloaded = false;
+      _downloadingFile = false;
+      _unprocessFile = true;
+    });
+  }
+
+  void setStateDownloading() {
+    setState(() {
+      _progress = '0';
+      _askUnprocessFile = false;
+      _isDownloaded = false;
+      _downloadingFile = true;
+      _unprocessFile = false;
+    });
+  }
+
+  void setStateFinishDownload() {
+    setState(() {
+      _progress = '100';
+      _askUnprocessFile = false;
+      _isDownloaded = true;
+      _downloadingFile = false;
+      _unprocessFile = false;
+    });
+  }
+
+  void setStateReset() {
+    setState(() {
+      _progress = '0';
+      _askUnprocessFile = false;
+      _isDownloaded = false;
+      _downloadingFile = false;
+      _unprocessFile = false;
+    });
+  }
+
+  Future<FileBasicInformation> requestUnprocessFile(FileBasicInformation fileBasicInformation, TaskUnprocessFile taskUnprocessFile) async {
+    FileBasicInformation fileUnprocessed = await _workerService.unprocessFile(fileUuid: fileBasicInformation.uuid, taskUnprocessFile: taskUnprocessFile);
+    setState(() {
+      _progress = '0';
+      _askUnprocessFile = false;
+      _isDownloaded = false;
+      _downloadingFile = false;
+    });
+    return fileUnprocessed;
+  }
+
+  void checkIfFileIsTreated(FileBasicInformation fileBasicInformation) async {
+    _timer = Timer.periodic(Duration(seconds: 5), (timer) async {
+      _fileUnprocessed = await _fileService.getInformation(fileBasicInformation.uuid);
+      print("Checking file being processed to be downloaded");
+      if (_fileUnprocessed.isError) {
+        DialogHelper.displayDialog(context, "Download", "Cannot unprocess file yet, an error as occured, please retry later");
+        setStateReset();
+      }
+      if (_fileUnprocessed.isTreated) {
+        timer.cancel();
+        downloadFile(_fileUnprocessed);
+      }
+    });
+  }
 
   Widget makeFormUnprocess(FileAdvancedInformation fileAdvancedInformation) =>
       Padding(
@@ -174,7 +306,7 @@ class _FileDownloaderPage extends State<FileDownloaderPage> {
         child: Form(
           key: _formKey,
           child: Column(
-            children: formPasswordInputs,
+            children: _formPasswordInputs,
           ),
         ),
       );
@@ -195,6 +327,7 @@ class _FileDownloaderPage extends State<FileDownloaderPage> {
           autocorrect: false,
           keyboardType: keyboardType,
           validator: validator,
+          autovalidate: true,
           onSaved: onSaved,
           decoration: InputDecoration(
             contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
@@ -252,6 +385,53 @@ class _FileDownloaderPage extends State<FileDownloaderPage> {
                     progressColor: Colors.green,
                     animateFromLastPercent: true,
                   ),
+                ],
+              ),
+            ),
+            /*SizedBox(
+              height: 30,
+            ),*/
+            //makeButtonLinkToFileOnSystem()
+          ],
+        ),
+      );
+
+  Container makeContainerShowStatusFileUnprocess() =>
+      Container(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 25.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Unprocess Information',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Column(
+                children: <Widget>[
+                  Align(
+                    child: Text(
+                      'Status process: Waiting unprocess your file',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    alignment: Alignment.centerLeft,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: CircularProgressIndicator(
+                    ),
+                  )
                 ],
               ),
             ),
